33b2e4f8dc4cebc8878bf918ea07e18e35ade8d3
Artem Ishchenko <proteus11@ukr.net>
2020-03-30 13:47:33 +0300

    w5 - Application Integration: SQS, SNS

---
+++ w5/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_sns_topic" "user_updates" {
+  name = "user-updates-topic"
+}
+
+resource "aws_sqs_queue" "user_updates_queue" {
+  name = "user-updates-queue"
+}
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = aws_sns_topic.user_updates.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.user_updates_queue.arn
+}
+
+resource "aws_sns_topic_subscription" "techdb_email_alerts_sub" {
+  topic_arn = aws_sns_topic.user_updates.arn
+  protocol = "sms"
+  endpoint = "XXXXXXXX"
+}
